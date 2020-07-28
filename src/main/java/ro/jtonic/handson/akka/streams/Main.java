8abb2953530001b3867e434498ad300818e175a2
jtonic <antonel.pazargic@gmail.com>
2020-07-28 08:42:18 +0300

    [akka-streams] springboot main app

---
+++ src/main/java/ro/jtonic/handson/akka/streams/Main.java
@@ -0,0 +1,25 @@
+package ro.jtonic.handson.akka.streams;
+
+import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Main {
+
+  public static void main(String[] args) {
+    new SpringApplicationBuilder()
+        .sources(Main.class)
+        .web(WebApplicationType.NONE)
+        .bannerMode(Mode.OFF)
+        .build().run(args);
+  }
+
+  @Bean
+  public static CommandLineRunner runner() {
+    return args -> System.out.println("Run the application");
+  }
+}
